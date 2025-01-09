14475a0c237b5fe68a2947dd0a56766efcdfdb17
elora5 <elorakhan033@gmail.com>
2025-01-09 13:25:25 -0800

    Add files via upload

--- Mis_final/script.js
+++ Mis_final/script.js
@@ -4,7 +4,4 @@ const mealResults = document.getElementById("meal-results");
 const showAllContainer = document.getElementById("show-all-container");
 
 async function fetchMeals(query) {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.meals || []
+    const url = 
